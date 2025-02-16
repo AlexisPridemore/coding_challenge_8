cd656784dcd44c6dcbc84c1c9748903d69997cad
Alexis Pridemore <alpridemore@usf.edu>
2025-02-16 14:34:46 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -7,3 +7,13 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 
 console.log(calculateSalary(5000, 500, 0.1));     //Test Data
 console.log(calculateSalary(7000, 1000, 0.15));   //Test Data
+
+// Task 2 Function Expression, Scenario: Product Price After Discount
+
+const calculateDiscount =function(price, discountRate) {   //add discount
+    let finalPrice = price - (price * discountRate);
+    return `Final Price: $${finalPrice.toFixed(2)}`;
+}; //calculate final price
+
+console.log(calculateDiscount(100, 0.2));   //Test Data
+console.log(calculateDiscount(250, 0.15));  //Test Data
